60ca5230d4a2f9950705f87a7a8277c244165844
rbaiadori92 <rbaiadori@outlook.com>
2025-07-22 16:26:46 +0100

    Ligação e encerramento com a base de dados.

--- app.php
+++ app.php
@@ -2,12 +2,21 @@
 
 // CRUD = Create Read Update Delete
 
-// Criar uma conexão à base dados
+// Criar uma conexão à base de dados
 $con = mysqli_connect('127.0.0.1', 'root', '', 'recipe_app_dump');
 
-//verificar se a conexão foi concluida
+// Verificar se a conexão foi concluída
 if ($con) {
     echo "Conexão com a base de dados concluída!\n";
 } else {
     echo "Erro na conexão com a base de dados\n";
+    exit(); // Encerra o script se não houver conexão
 }
+
+$fim = true;
+
+mysqli_close($con);
+echo "Conexão com a base de dados encerrada.\n";
+
+
+
